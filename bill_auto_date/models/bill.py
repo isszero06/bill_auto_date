bc82187bd8b286f6513183950b032ab4056f236c
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-11-10 01:46:10 +0200

    Update bill.py

--- bill_auto_date/models/bill.py
+++ bill_auto_date/models/bill.py
@@ -21,12 +21,10 @@ class AccountMove(models.Model):
 
     def _post(self, soft=False):
         for move in self:
-
-            if not move.invoice_date:
-                if move.is_sale_document(include_receipts=True):
+                if move.is_sale_document(include_receipts=True) and not move.invoice_date:
                     move.invoice_date = fields.Date.context_today(self)
                     move.with_context(check_move_validity=False)._onchange_invoice_date()
-                elif move.is_purchase_document(include_receipts=True):
+                elif move.is_purchase_document(include_receipts=True) and not move.invoice_date:
                     move.invoice_date = fields.Date.context_today(self)
                 move.invoice_date = fields.Date.context_today(move)
         res = super(AccountMove, self)._post(soft=False)
